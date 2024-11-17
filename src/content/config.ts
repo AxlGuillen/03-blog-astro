9ad521ea9d1e49450663ffaae4f3d2eff53e2d76
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-11-16 18:48:44 -0600

    Relacion entre authores y posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -11,12 +11,21 @@ const blogCollection = defineCollection({
     }),
     
     //relacion
-    author: z.string(),
+    author: reference("author"),
     //relacion
     tags: z.array(z.string()),
   }),
 });
 
+  const authorCollection = defineCollection({
+    type: "data",
+    schema: ({ image }) => z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+  })
+
 export const collections = {
     blog: blogCollection,
+    author: authorCollection
 }
